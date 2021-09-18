8b23d2b710118dcb0729efc4f91ca5ddf8ba2576
A Alver <wodafone@hotmail.com>
2021-09-18 16:31:49 +0100

    I finished first part of my project

--- src/test/java/com/moneygaming/tests/Test1.java
+++ src/test/java/com/moneygaming/tests/Test1.java
@@ -1,23 +1,34 @@
 package com.moneygaming.tests;
 
 import com.moneygaming.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Test1 {
 
+    WebDriver driver;
+
     @BeforeMethod
     public void setUp () {
 
     WebDriver driver = WebDriverFactory.getDriver("chrome");
     driver.manage().window().maximize();
+    //get the page
+    driver.get("https://moneygaming.qa.gameaccount.com/sign-up.shtml");
 
     }
 
     @Test
-    public void test1 () {
+    public void test1 () throws InterruptedException {
+        WebElement nameBox = driver.findElement(By.cssSelector("#forename"));
+        nameBox.sendKeys("Mike");
+        WebElement surnameBox = driver.findElement(By.xpath("//input[@name='map(lastName)']"));
+        surnameBox.sendKeys("Smith");
+        Thread.sleep(2000);
 
     }
 
