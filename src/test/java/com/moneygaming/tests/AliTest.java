a6fab4ea34e771c99fbb03e9c76844bddfdbb583
A Alver <wodafone@hotmail.com>
2021-09-18 17:36:42 +0100

    Ali branch commit

---
+++ src/test/java/com/moneygaming/tests/AliTest.java
@@ -0,0 +1,11 @@
+package com.moneygaming.tests;
+
+import org.testng.annotations.Test;
+
+public class AliTest {
+
+    @Test
+    public void testAli() {
+        System.out.println("my beautiful test");
+    }
+}
