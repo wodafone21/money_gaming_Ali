1422c764a6295ba780191146e2ddd39ce3244e61
A Alver <wodafone@hotmail.com>
2021-09-20 07:01:08 +0100

    Amazon webpage inputBox added

--- src/test/java/com/moneygaming/tests/Test1.java
+++ src/test/java/com/moneygaming/tests/Test1.java
@@ -4,6 +4,7 @@ import com.moneygaming.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -13,17 +14,17 @@ public class Test1 {
     WebDriver driver;
 
     @BeforeMethod
-    public void setUp () {
+    public void setUp() {
 
-    driver = WebDriverFactory.getDriver("chrome");
-    driver.manage().window().maximize();
-    //get the page
-    driver.get("https://moneygaming.qa.gameaccount.com/sign-up.shtml");
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        //get the page
+        driver.get("https://moneygaming.qa.gameaccount.com/sign-up.shtml");
 
     }
 
     @Test
-    public void test1 () throws InterruptedException {
+    public void test1() throws InterruptedException {
         WebElement nameBox = driver.findElement(By.cssSelector("#forename"));
         nameBox.sendKeys("Mike");
         WebElement surnameBox = driver.findElement(By.xpath("//input[@name='map(lastName)']"));
@@ -32,8 +33,22 @@ public class Test1 {
 
     }
 
-    @AfterMethod
-    public void tearDown () {
+    @Test
+    public void test2() {
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.get("https://www.amazon.co.uk/");
+        WebElement inputBox = driver.findElement(By.cssSelector("#twotabsearchtextbox"));
+        inputBox.sendKeys("SamsungQLED TV");
+        WebElement clickButton = driver.findElement(By.cssSelector("#nav-search-submit-button"));
+        clickButton.click();
+
+
 
     }
+
+    @AfterMethod
+    public void tearDown() {
+
+    }
+
 }
