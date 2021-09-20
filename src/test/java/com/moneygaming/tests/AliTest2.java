a2dfc88ce2d73c7c9c33b6ad0b2e49876b023658
A Alver <wodafone@hotmail.com>
2021-09-20 07:06:01 +0100

    Amazon webpage inputBox added again

---
+++ src/test/java/com/moneygaming/tests/AliTest2.java
@@ -0,0 +1,21 @@
+package com.moneygaming.tests;
+
+import com.moneygaming.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class AliTest2 {
+
+    @Test
+    public void test2() {
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.get("https://www.amazon.co.uk/");
+        WebElement inputBox = driver.findElement(By.cssSelector("#twotabsearchtextbox"));
+        inputBox.sendKeys("SamsungQLED TV");
+        WebElement clickButton = driver.findElement(By.cssSelector("#nav-search-submit-button"));
+        clickButton.click();
+    }
+
+}
